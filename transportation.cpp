7ab2b2f3ed7e0746ec88a0c307e24efec3863ba7
wntmorder <79230634+wntmorder@users.noreply.github.com>
2021-02-18 18:34:46 +0300

    Update transportation.cpp

--- transportation.cpp
+++ transportation.cpp
@@ -1,16 +1,14 @@
-#include (iostream);
+#include <iostream>
 
-using namespace std; 
+using namespace std;
 
- setlocal (LC_ALL, 'Russian')
+int main() {
+   
+   int BusCapacity = 50;
+   int Passengers;
 
-int main () {
-	
-    int x=50;
-	int y;
-	cout << ('Колличество посажиров на остановке: ');
-	cin >> (y);
-	cout<<('Колличество свободных мест в автобусе: ') <<x-(y%x);
-	
-	return 0;
+   cin >> (Passengers);
+   cout << BusCapacity - (Passengers % BusCapacity);
+
+return 0;
 }
