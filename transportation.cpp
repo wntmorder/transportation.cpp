89a5f29f9a8e9143afc42865c95d37f0b030db3d
wntmorder <79230634+wntmorder@users.noreply.github.com>
2021-02-19 10:23:01 +0300

    Create transportation.cpp

---
+++ transportation.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+   string s1  ("Введите количество пассажиров на остановке:");
+   string s2 ("Количество свободных мест в автобусе: ");
+	
+   
+   int BusCapacity = 50;
+   int Passengers;
+	
+   cout << s1 << endl;
+   cin >> (Passengers);
+   cout << s2 << BusCapacity - (Passengers % BusCapacity);
+
+return 0;
+}
